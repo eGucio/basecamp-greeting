35fe2a9049e4b735989d8ef0f80a39c17126ce2e
eGucio <egucio70@gmail.com>
2024-10-27 14:04:47 +0100

    say your name

--- contracts/greeting.sol
+++ contracts/greeting.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.7;
 
-contract greeting {
+contract greeting { 
     function sayHello() external pure returns (string memory) {
         return "Hello World";
     }
+
+    // Przykład złego kodu: nie działa
+    // function Greeter(string memory _name) external pure returns (string memory) {
+    // return "Hello " + _name + "!";
+    // }
+    
+    // Przykład dzaiłającego kodu
+    function Greeter(string memory _name) external pure returns (string memory, string memory) {
+    return ("Hello ", _name);
+}
 }
